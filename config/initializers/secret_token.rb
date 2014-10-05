61cd0e9df946365314153acbf1b94d55d8b9cdb2
Mamiya ENT Clinic <mamiya.ent@gmail.com>
2014-10-05 11:32:37 +0900

    rake rails:update 直後

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Audioserver::Application.config.secret_token = 'f063f1effa4fe0b5eede38fd1a034457841cebbae80172ee74559d1a1f4ae1331fb5dab5c5a0e19af9262fbfe95fe9010eee736a5f422b1cb04a7fa6cbd5c1d0'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Audioserver::Application.config.secret_key_base = '47f9c1b39aa8b6c4c7928055a55eadcdcb6752a06105120851ae30d4997561c69d2a5a1556c4428ac01ac1988d3ebfd3b94dca1186b7d1a89c0326f373015218'
